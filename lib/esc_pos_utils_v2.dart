48467261d6cec3f00098f3e880782201d6b67248
Oeng Mengthong <40915642+oengmengthong@users.noreply.github.com>
2022-11-25 11:32:32 +0700

    update

---
+++ lib/esc_pos_utils_v2.dart
@@ -0,0 +1,17 @@
+/*
+ * esc_pos_utils_v2
+ * Created by Oeng Mengthong.
+ * 
+ * Copyright (c) 2019-2020. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+library esc_pos_utils_v2;
+
+export './src/barcode.dart';
+export './src/capability_profile.dart';
+export './src/enums.dart';
+export './src/pos_column.dart';
+export './src/pos_styles.dart';
+export './src/qrcode.dart';
+export './src/generator.dart';
---
+++ test/esc_pos_utils_v2_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:esc_pos_utils_v2/esc_pos_utils_v2.dart';
+
+void main() {
+  test('Tests not implemented', () {
+    expect(1, 1);
+  });
+}
